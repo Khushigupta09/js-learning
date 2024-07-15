65ca28348ba47d4972304e393d9cf0cb4af22705
Khushi Gupta <108022410+Khushigupta09@users.noreply.github.com>
2024-07-15 14:45:39 +0000

    strings done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,73 @@
+// const name ="khushi"
+// console.log(name);
+// console.log(`hello my name is ${name}`);
+
+// const name2 =new String('gupta')
+// console.log(name2);
+// //  console.log(name2.small());
+
+//  const name3 = name2.startsWith('pt',2);
+//  console.log(name3);
+
+ const str = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(str.slice(31));
+// Expected output: "the lazy dog."
+
+console.log(str.slice(4, 19));
+// Expected output: "quick brown fox"
+
+console.log(str.slice(-4));
+// Expected output: "dog."
+
+console.log(str.slice(-9, -5));
+// Expected output: "lazy"
+
+
+const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const str1 = "The morning is upon us.";
+
+str.slice(-3); // 'us.'
+str.slice(-3, -1); // 'us'
+str.slice(0, -1); // 'The morning is upon us'
+str.slice(4, -1); // 'morning is upon us'
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
+
+
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+const word = 'fox';
+
+console.log(
+  `The word "${word}" ${
+    sentence.includes(word) ? 'is' : 'is not'
+  } in the sentence`,
+);
+// Expected output: "The word "fox" is in the sentence"
